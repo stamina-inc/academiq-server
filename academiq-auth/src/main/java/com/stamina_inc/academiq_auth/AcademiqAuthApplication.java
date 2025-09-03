7670c78f40ed1a7f33c655103b2d09cd6e87bf91
Adam Mehdaoui <adam.mehdaouijorge@gmail.com>
2025-09-03 09:58:27 +0200

    Initial commit

---
+++ academiq-auth/src/main/java/com/stamina_inc/academiq_auth/AcademiqAuthApplication.java
@@ -0,0 +1,13 @@
+package com.stamina_inc.academiq_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademiqAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademiqAuthApplication.class, args);
+	}
+
+}
---
+++ academiq-auth/src/test/java/com/stamina_inc/academiq_auth/AcademiqAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stamina_inc.academiq_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademiqAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
