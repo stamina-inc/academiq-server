4f14e1d390176c3eca418cb852b55acab1670599
Adam Mehdaoui <adam.mehdaouijorge@gmail.com>
2025-09-04 10:23:39 +0200

    chore: gateway setup

---
+++ academiq-auth/src/main/java/com/stamina_inc/academiq_auth/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.stamina_inc.academiq_auth.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping
+    public ResponseEntity<String> test() {
+        return ResponseEntity.ok("OK");
+    }
+
+}
---
+++ academiq-auth/src/main/java/com/stamina_inc/academiq_auth/infrastructure/KafkaPublisher.java
@@ -0,0 +1,4 @@
+package com.stamina_inc.academiq_auth.infrastructure;
+
+public class KafkaPublisher {
+}
